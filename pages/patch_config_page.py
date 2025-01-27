7c038eb71e8c7725a649076f1eb7daa376aeec4e
KZL <31285862+KZL00@users.noreply.github.com>
2025-01-27 22:40:49 +0100

    implemented UI

--- pages/patch_config_page.py
+++ pages/patch_config_page.py
@@ -6,5 +6,54 @@ class PatchConfigPage:
 
     def build(self):
         return ft.Column([
-            ft.Text("PatchConfigPage")
+            ft.Row([
+                ft.Text(
+                    "Configure Patch",
+                    size=24,
+                )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            ),
+            ft.Card(
+                content=ft.Container(
+                    content=ft.Column([
+                        ft.ListTile(
+                            leading=ft.Icon(ft.icons.WARNING),
+                            title=ft.Text("Before You Start"),
+                            subtitle=ft.Text("To ensure everything works correctly, please make sure your game is on the pre-20th anniversary version, also known as the steam_legacy branch. This version of Half-Life 2 is required for HL2Patcher to function properly.\n\nYou can switch to the steam_legacy branch through the Steam client by right-clicking on the game, selecting “Properties”, and then choosing the appropriate branch under the “Betas” tab.\n\nOnce you’re all set, you can proceed with patching!")
+                        )
+                    ])
+                )
+            ),
+            ft.Card(
+                content=ft.Container(
+                    content=ft.Column([
+                        ft.ListTile(
+                            leading=ft.Icon(ft.icons.FOLDER),
+                            title=ft.Text("Game install directory"),
+                            subtitle=ft.Text("Enter the path to your Half-Life 2 install directory. You can find it by right-clicking the game in Steam, selecting “Properties”, then “Local Files”, and clicking “Browse”.")
+                        ),
+                        ft.Row(
+                            controls=[
+                                ft.TextField(
+                                    expand=True,
+                                    read_only=True
+                                ),
+                                ft.IconButton(
+                                    icon=ft.icons.FOLDER,
+                                )
+                            ]
+                        )
+                    ]),
+                    padding=10
+                )
+            ),
+            ft.Row([
+                ft.TextButton(
+                    "Patch",
+                    icon=ft.icons.INSTALL_DESKTOP,
+                )
+            ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            )
         ])
--- pages/start_page.py
+++ pages/start_page.py
@@ -7,5 +7,73 @@ class StartPage:
 
     def build(self):
         return ft.Column([
-            ft.Text('StartPage'),
+            ft.Card(
+                content=ft.Container(
+                    content=ft.Column([
+                        ft.ListTile(
+                            leading=ft.Image(
+                                src=f"../assets/icon.png",
+                                fit=ft.ImageFit.CONTAIN
+                            ),
+                            title=ft.Text('Welcome to HL2Patcher'),
+                            subtitle=ft.Text('HL2Patcher makes Half-Life 2 playable on modern ARM Macs that only support 64-bit apps. Its goal is to simplify the process into an easy-to-use app, so anyone can enjoy the game again without hassle.'),
+                        )
+                    ])
+                )
+            ),
+            ft.Card(
+                content=ft.Container(
+                    content=ft.Column([
+                        ft.ListTile(
+                            leading=ft.Icon(ft.icons.WARNING),
+                            title=ft.Text("Liability Disclaimer"),
+                            subtitle=ft.Text("By using HL2Patcher, you acknowledge that you do so at your own risk. While the app is designed to help, the developer cannot be held responsible for any issues, including data loss, system instability, or other unintended consequences."),
+                        )
+                    ])
+                )
+            ),
+            ft.Card(
+                content=ft.Container(
+                    content=ft.Column([
+                        ft.ListTile(
+                            leading=ft.Icon(ft.icons.BUILD),
+                            title=ft.Text("Required Tools"),
+                            subtitle=ft.Text("To use HL2Patcher, you’ll need a few essential tools to ensure everything runs smoothly. These tools are required to properly patch the game.\n\nMake sure to check that you have all the necessary components before continuing. If you need help to install any of theese tools click question mark."),
+                        ),
+                        ft.ListTile(
+                            leading=ft.Icon(ft.icons.CHECK_CIRCLE),
+                            title=ft.Text("Xcode"),
+                            subtitle=ft.Text("Required for compiling files and ensuring the app works correctly."),
+                            trailing=ft.Icon(ft.icons.HELP_ROUNDED)
+                        ),
+                        ft.ListTile(
+                            leading=ft.Icon(ft.icons.REMOVE_CIRCLE),
+                            title=ft.Text("Xcode Command Line Tools"),
+                            subtitle=ft.Text("Needed for various build tasks."),
+                            trailing=ft.Icon(ft.icons.HELP_ROUNDED)
+                        ),
+                        ft.ListTile(
+                            leading=ft.Icon(ft.icons.REMOVE_CIRCLE),
+                            title=ft.Text("HomeBrew"),
+                            subtitle=ft.Text("A package manager used to install necessary dependencies."),
+                            trailing=ft.Icon(ft.icons.HELP_ROUNDED)
+                        ),
+                        ft.ListTile(
+                            leading=ft.Icon(ft.icons.REMOVE_CIRCLE),
+                            title=ft.Text("Dependencies"),
+                            subtitle=ft.Text("Additional tools required by HL2Patcher, installed through Homebrew."),
+                            trailing=ft.Icon(ft.icons.HELP_ROUNDED)
+                        )
+                    ])
+                )
+            ),
+            ft.Row([
+                ft.TextButton(
+                    "Next",
+                    icon=ft.icons.ARROW_RIGHT,
+                    on_click=lambda e: self.nav_service.navigate_by_name("patch_config")
+                )
+            ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            )
         ])
