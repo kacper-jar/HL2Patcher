5ebadd9d16d8c880333f609b4acf4f378dcd6fb1
KZL <31285862+KZL00@users.noreply.github.com>
2025-03-06 20:57:35 +0100

    implemented UI and logic NotSupportedPage

--- pages/not_supported_page.py
+++ pages/not_supported_page.py
@@ -6,5 +6,30 @@ class NotSupportedPage:
 
     def build(self):
         return ft.Column([
-            ft.Text("NotSupportedPage")
+            ft.Card(
+                content=ft.Container(
+                    content=ft.Column([
+                        ft.ListTile(
+                            leading=ft.Icon(ft.icons.WARNING),
+                            title=ft.Text("Unsupported OS"),
+                            subtitle=ft.Text("HL2Patcher is only compatible with macOS 11 or newer. Running it on an unsupported system will prevent it from working properly. Please update your macOS or use a supported device."),
+                        ),
+                        ft.Row(
+                            controls=[
+                                ft.TextButton(
+                                    icon=ft.icons.EXIT_TO_APP,
+                                    text="Exit",
+                                    on_click=lambda e: self.handle_exit()
+                                )
+                            ],
+                            alignment=ft.MainAxisAlignment.END,
+                        )
+                    ]),
+                    padding=10
+                ),
+                expand=True
+            )
         ])
+
+    def handle_exit(self):
+        self.app.page.window.close()
